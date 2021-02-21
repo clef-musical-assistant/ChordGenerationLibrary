d4f5b3bcbe73c3a31af31f541b73a76f6c13230d
Dustin Thompson <dustin.ct1996@gmail.com>
2021-02-21 14:21:36 -0500

    v 1.0.6

--- clefchordgenerator.js
+++ clefchordgenerator.js
@@ -1,23 +1,29 @@
-exports.generateChordProgression = function(chosenKey, chosenAdjective) {
+exports.generateChordProgression = function(chosenKey, chosenOperator, chosenAdjective) {
     var keys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-
+    var majorRomanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+    var minorRomanNumerals = ['i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii'];
     var majorProgression = [0, 2, 2, 1, 2, 2, 2];
     var minorProgression = [0, 2, 1, 2, 2, 1, 2];
     var chordsInProgression = [];
-    var chordProgression = "";
-    var counter = 0, ptr;
+    var chordProgression = [];
     let picked = new Set();
+    var counter = 0, ptr;
 
-    const generateMajorProgression = function(chosenKey)
+    const generateMajorProgression = function()
     {
         var chordsInProgression = [];
         var ptr;
 
-        chordsInProgression.push(chosenKey + "M");
-
         for (ptr = 0; ptr < 12; ptr++)
             if (keys[ptr] == chosenKey)
                 break;
+
+        if (chosenOperator == "#")
+            ptr = (ptr + 1) % 12;
+        else if (chosenOperator == '\u266D')
+            ptr = (ptr - 1) % 12;
+
+        chordsInProgression.push(keys[ptr]);
 
         for (var i = 1; i < 7; i++)
         {
@@ -28,22 +34,27 @@ exports.generateChordProgression = function(chosenKey, chosenAdjective) {
             else if (6 % (i + 1) == 0)
                 chordsInProgression.push(keys[ptr % 12] + "m");
             else
-                chordsInProgression.push(keys[ptr % 12] + "M");
+                chordsInProgression.push(keys[ptr % 12]);
         }
 
         return chordsInProgression;
     }
 
-    const generateMinorProgression = function(chosenKey)
+    const generateMinorProgression = function()
     {
         var chordsInProgression = [];
         var ptr;
 
-        chordsInProgression.push(chosenKey + "m");
-
         for (ptr = 0; ptr < 12; ptr++)
             if (keys[ptr] == chosenKey)
                 break;
+
+        if (chosenOperator == "#")
+            ptr = (ptr + 1) % 12;
+        else if (chosenOperator == '\u266D')
+            ptr = (ptr - 1) % 12;
+
+        chordsInProgression.push(keys[ptr] + "m");
 
         for (var i = 1; i < 7; i++)
         {
@@ -54,16 +65,20 @@ exports.generateChordProgression = function(chosenKey, chosenAdjective) {
             else if (20 % (i + 1) == 0)
                 chordsInProgression.push(keys[ptr % 12] + "m");
             else
-                chordsInProgression.push(keys[ptr % 12] + "M");
+                chordsInProgression.push(keys[ptr % 12]);
         }
 
         return chordsInProgression;
     }
 
-    if (chosenAdjective == "M")
-        chordsInProgression = generateMajorProgression(chosenKey);
+    if (chosenAdjective == "Major")
+        chordsInProgression = generateMajorProgression();
     else
-        chordsInProgression = generateMinorProgression(chosenKey);
+        chordsInProgression = generateMinorProgression();
+
+    chordProgression.push("");
+    chordProgression.push("");
+    chordProgression.push("");
 
     while (counter < 4)
     {
@@ -73,7 +88,19 @@ exports.generateChordProgression = function(chosenKey, chosenAdjective) {
         {
             picked.add(ptr);
 
-            chordProgression += chordsInProgression[ptr];
+            chordProgression[0] += chordsInProgression[ptr];
+            chordProgression[2] += chordsInProgression[ptr];
+ 
+            if (chordsInProgression[ptr].includes('m') || chordsInProgression[ptr].includes('dim'))
+                chordProgression[1] += minorRomanNumerals[ptr];
+            else
+                chordProgression[1] += majorRomanNumerals[ptr];
+
+            if(counter != 3)
+            {
+                chordProgression[0] += ' - ';
+                chordProgression[1] += ' - ';
+            }
 
             counter++;
         }
