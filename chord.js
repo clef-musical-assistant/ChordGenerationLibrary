383bcf28f12d01f72132340e42fe2f6fedd663c4
dustinct1996 <50458389+dustinct1996@users.noreply.github.com>
2021-04-20 12:40:12 -0400

    Update chord.js

--- chord.js
+++ chord.js
@@ -12,27 +12,27 @@ exports.get = function(chord)
 
     if(chord.includes('dim'))   
     {
-        ptr = chord.includes('#') ? coreTheory.theory.keysSharp.roots.findIndex((element) => element === trimmedChord) : coreTheory.theory.keysFlat.roots.findIndex((element) => element === trimmedChord);
+        ptr = chord.includes('#') ? coreTheory.theory.keys.sharp.findIndex((element) => element === trimmedChord) : coreTheory.theory.keys.flat.findIndex((element) => element === trimmedChord);
 
-        chordInfo.notes.push(coreTheory.theory.keysFlat.roots[ptr]);
-        chordInfo.notes.push(coreTheory.theory.keysFlat.roots[(ptr + 3) % 12]);
-        chordInfo.notes.push(coreTheory.theory.keysFlat.roots[(ptr + 6) % 12]);
+        chordInfo.notes.push(coreTheory.theory.keys.flat[ptr]);
+        chordInfo.notes.push(coreTheory.theory.keys.flat[(ptr + 3) % 12]);
+        chordInfo.notes.push(coreTheory.theory.keys.flat[(ptr + 6) % 12]);
     }
     else if(chord.includes('m'))
     {
-        ptr = chord.includes('#') ? coreTheory.theory.keysSharp.roots.findIndex((element) => element === trimmedChord) : coreTheory.theory.keysFlat.roots.findIndex((element) => element === trimmedChord);
+        ptr = chord.includes('#') ? coreTheory.theory.keys.sharp.findIndex((element) => element === trimmedChord) : coreTheory.theory.keys.flat.findIndex((element) => element === trimmedChord);
 
-        chordInfo.notes.push(coreTheory.theory.keysFlat.roots[ptr]);
-        chordInfo.notes.push(coreTheory.theory.keysFlat.roots[(ptr + 3) % 12]);
-        chordInfo.notes.push(coreTheory.theory.keysFlat.roots[(ptr + 7) % 12]);
+        chordInfo.notes.push(coreTheory.theory.keys.flat[ptr]);
+        chordInfo.notes.push(coreTheory.theory.keys.flat[(ptr + 3) % 12]);
+        chordInfo.notes.push(coreTheory.theory.keys.flat[(ptr + 7) % 12]);
     }
     else
     {
-        ptr = chord.includes('#') ? coreTheory.theory.keysSharp.roots.findIndex((element) => element === trimmedChord) : coreTheory.theory.keysFlat.roots.findIndex((element) => element === trimmedChord);
+        ptr = chord.includes('#') ? coreTheory.theory.keys.sharp.findIndex((element) => element === trimmedChord) : coreTheory.theory.keys.flat.findIndex((element) => element === trimmedChord);
 
-        chordInfo.notes.push(coreTheory.theory.keysSharp.roots[ptr]);
-        chordInfo.notes.push(coreTheory.theory.keysSharp.roots[(ptr + 4) % 12]);
-        chordInfo.notes.push(coreTheory.theory.keysSharp.roots[(ptr + 7) % 12]);
+        chordInfo.notes.push(coreTheory.theory.keys.sharp[ptr]);
+        chordInfo.notes.push(coreTheory.theory.keys.sharp[(ptr + 4) % 12]);
+        chordInfo.notes.push(coreTheory.theory.keys.sharp[(ptr + 7) % 12]);
     }
 
     return chordInfo;
