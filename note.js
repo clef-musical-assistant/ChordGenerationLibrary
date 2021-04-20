735bdb56ab8bfa742c46d6abd528a1b0d7bc857b
dustinct1996 <50458389+dustinct1996@users.noreply.github.com>
2021-04-20 12:40:43 -0400

    Update note.js

--- note.js
+++ note.js
@@ -5,7 +5,7 @@ exports.midi = function(note, octave = 0)
     var midiVal = coreTheory.theory.midi.A0;
     var index;
 
-    index = note.includes('#') ? coreTheory.theory.keysSharp.roots.findIndex((element) => element === note) : coreTheory.theory.keysFlat.roots.findIndex((element) => element === note);
+    index = note.includes('#') ? coreTheory.theory.keys.sharp.findIndex((element) => element === note) : coreTheory.theory.keys.flat.findIndex((element) => element === note);
 
     midiVal += (12 * octave) + (index - 9);
 
